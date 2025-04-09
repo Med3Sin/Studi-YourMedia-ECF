a66de22b7f2bcdd0a3d48a95b8ff5c4430507aaa
0MedSin0 <0medsin0@gmail.com>
2025-04-09 18:06:29 +0200

    Correction des descriptions des règles de groupe de sécurité pour supprimer les caractères non autorisés

--- infrastructure/modules/network/main.tf
+++ infrastructure/modules/network/main.tf
@@ -20,7 +20,7 @@ resource "aws_security_group_rule" "ec2_ingress_ssh" {
   protocol          = "tcp"
   cidr_blocks       = [var.operator_ip] # Restreint à l'IP fournie
   security_group_id = aws_security_group.ec2_sg.id
-  description       = "Autoriser SSH depuis l'IP de l'opérateur"
+  description       = "Autoriser SSH depuis IP operateur"
 }
 
 # Règle entrante: HTTP (pour accès web standard si besoin)
@@ -31,7 +31,7 @@ resource "aws_security_group_rule" "ec2_ingress_http" {
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"] # Ouvert à tous
   security_group_id = aws_security_group.ec2_sg.id
-  description       = "Autoriser le trafic HTTP"
+  description       = "Autoriser trafic HTTP"
 }
 
 # Règle entrante: Tomcat (port par défaut)
@@ -42,7 +42,7 @@ resource "aws_security_group_rule" "ec2_ingress_tomcat" {
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"] # Ouvert à tous pour l'API
   security_group_id = aws_security_group.ec2_sg.id
-  description       = "Autoriser le trafic Tomcat (API)"
+  description       = "Autoriser trafic Tomcat API"
 }
 
 # Règle entrante: Prometheus Actuator Endpoint (depuis le SG ECS)
@@ -53,7 +53,7 @@ resource "aws_security_group_rule" "ec2_ingress_prometheus_actuator" {
   protocol                 = "tcp"
   source_security_group_id = aws_security_group.ecs_sg.id # Autorise depuis le SG ECS
   security_group_id        = aws_security_group.ec2_sg.id
-  description              = "Autoriser le scraping Prometheus depuis le SG ECS"
+  description              = "Autoriser scraping Prometheus depuis SG ECS"
 }
 
 # Règle sortante: Autorise tout le trafic sortant
@@ -64,7 +64,7 @@ resource "aws_security_group_rule" "ec2_egress_all" {
   protocol          = "-1" # Tout protocole
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.ec2_sg.id
-  description       = "Autoriser tout le trafic sortant"
+  description       = "Autoriser tout trafic sortant"
 }
 
 
@@ -90,7 +90,7 @@ resource "aws_security_group_rule" "rds_ingress_mysql" {
   protocol                 = "tcp"
   source_security_group_id = aws_security_group.ec2_sg.id # Autorise uniquement depuis le SG EC2
   security_group_id        = aws_security_group.rds_sg.id
-  description              = "Autoriser le trafic MySQL depuis le SG EC2"
+  description              = "Autoriser trafic MySQL depuis SG EC2"
 }
 
 # Règle sortante: Autorise tout le trafic sortant (généralement pas nécessaire pour RDS, mais sans danger)
@@ -101,7 +101,7 @@ resource "aws_security_group_rule" "rds_egress_all" {
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.rds_sg.id
-  description       = "Autoriser tout le trafic sortant"
+  description       = "Autoriser tout trafic sortant"
 }
 
 
@@ -110,7 +110,7 @@ resource "aws_security_group_rule" "rds_egress_all" {
 # -----------------------------------------------------------------------------
 resource "aws_security_group" "ecs_sg" {
   name        = "${var.project_name}-ecs-sg"
-  description = "Autorise le trafic entrant pour Grafana et sortant pour Prometheus"
+  description = "Autorise trafic entrant pour Grafana et sortant pour Prometheus"
   vpc_id      = var.vpc_id
 
   tags = {
@@ -127,7 +127,7 @@ resource "aws_security_group_rule" "ecs_ingress_grafana" {
   protocol          = "tcp"
   cidr_blocks       = [var.operator_ip] # Restreint à l'IP fournie
   security_group_id = aws_security_group.ecs_sg.id
-  description       = "Autoriser l'accès Grafana depuis l'IP de l'opérateur"
+  description       = "Autoriser acces Grafana depuis IP operateur"
 }
 
 # Règle sortante: Autorise tout le trafic sortant
@@ -139,5 +139,5 @@ resource "aws_security_group_rule" "ecs_egress_all" {
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.ecs_sg.id
-  description       = "Autoriser tout le trafic sortant"
+  description       = "Autoriser tout trafic sortant"
 }
