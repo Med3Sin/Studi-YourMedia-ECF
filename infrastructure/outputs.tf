6bb61f55463c5cdb98a5c0148ebd0b4cad45c415
0MedSin0 <0medsin0@gmail.com>
2025-04-13 18:47:41 +0200

    Marquer les outputs sensibles comme tels pour résoudre l'erreur de validation Terraform

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,6 +1,7 @@
 output "ec2_public_ip" {
   description = "Adresse IP publique de l'instance EC2 hébergeant le backend Java."
   value       = module.ec2-java-tomcat.public_ip
+  sensitive   = true
 }
 
 output "rds_endpoint" {
@@ -27,19 +28,23 @@ output "s3_bucket_name" {
 output "amplify_app_url" {
   description = "URL de l'application déployée sur AWS Amplify."
   value       = local.create_amplify_app ? "https://${aws_amplify_branch.main[0].branch_name}.${aws_amplify_app.frontend_app[0].default_domain}" : ""
+  sensitive   = true
 }
 
 output "monitoring_ec2_public_ip" {
   description = "Adresse IP publique de l'instance EC2 hébergeant Grafana et Prometheus."
   value       = module.ec2-monitoring.ec2_instance_public_ip
+  sensitive   = true
 }
 
 output "grafana_url" {
   description = "URL d'accès à Grafana."
   value       = module.ec2-monitoring.grafana_url
+  sensitive   = true
 }
 
 output "prometheus_url" {
   description = "URL d'accès à Prometheus."
   value       = module.ec2-monitoring.prometheus_url
+  sensitive   = true
 }
