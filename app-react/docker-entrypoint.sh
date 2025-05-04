b028bf4b89ecac8fc792e7a7215e2cc4660714d8
Med3Sin <0medsin0@gmail.com>
2025-05-04 16:09:54 +0200

    Correction du script d'entrée pour la compatibilité avec serve 14.2.4

--- app-react/docker-entrypoint.sh
+++ app-react/docker-entrypoint.sh
@@ -6,4 +6,6 @@ echo "Serve version:"
 serve --version
 
 # Démarrer serve avec les options appropriées
-exec serve -s build -p 3000 --host 0.0.0.0
+# Note: La version 14.2.4 de serve n'accepte pas l'option --host
+# Utiliser l'option -l avec l'adresse IP et le port
+exec serve -s build -l 3000
